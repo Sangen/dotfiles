5514d4d4c03e6e1b7c576bafed69c0991a261988
Kaoru Kawakita <sangendokei@gmail.com>
2015-03-13 23:43:46 +0900

    Add a command param to replace all as "-r"

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Command line options
+# -r : replace dot files if these are already exist
+while getopts r OPT
+do
+  case $OPT in
+    "r" ) REPLACE="TRUE" ;;
+      * ) ;;
+  esac
+done
+
+
 if [ -a $HOME/.vim/bundle ]; then
   echo ".vim/bundle is already exists."
 else
@@ -18,10 +29,16 @@ CURRENT=$(cd $(dirname $0) && pwd)
 for file in ${DOT_FILES[@]}
 do
   if [ -a $HOME/$file ]; then
-    echo "$file is already exists."
+    if [ "$REPLACE" = "TRUE" ]; then
+      rm $HOME/$file
+      ln -s -n $CURRENT/$file $HOME/$file
+      echo "replace a symbolic link to $file"
+    else
+      echo "$file is already exists."
+    fi
   else
     ln -s -n $CURRENT/$file $HOME/$file
-    echo "made a symbolic link to $file "
+    echo "made a symbolic link to $file"
   fi
 done
 
@@ -30,7 +47,12 @@ COPY_DOT_FILES=( .gitconfig )
 for file in ${COPY_DOT_FILES[@]}
 do
   if [ -a $HOME/$file ]; then
-    echo "$file is already exists."
+    if [ "$REPLACE" = "TRUE" ]; then
+      cp $CURRENT/$file $HOME/$file
+      echo "$file replaced"
+    else
+      echo "$file is already exists."
+    fi
   else
     cp $CURRENT/$file $HOME/$file
     echo "$file copied"
