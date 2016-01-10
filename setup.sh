2dcc322d93caf88805a303d9dcbe93f096813954
Sangendokei <sangendokei@gmail.com>
2016-01-10 11:33:01 +0900

    Add gibo completion file

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ fi
 mkdir ~/.vim/tmp ~/.vim/undo
 
 
-DOT_FILES=( .gitignore_global .vimrc .vrapperrc .xvimrc .zshrc .zshenv .jshintrc .screenrc )
+DOT_FILES=( .gitignore_global .vimrc .vrapperrc .xvimrc .zshrc .zshenv .jshintrc .screenrc .gibo-completion.zsh )
 
 CURRENT=$(cd $(dirname $0) && pwd)
 
